171878683d630577f69d886b87d67017a93599be
harshateja9597 <harshateja9597@gmail.com>
2024-04-29 12:42:50 +0530

    added 2 variables

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami = var.ami_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags={
     name = "Hello terraform"
--- variables/variables.tf
+++ variables/variables.tf
@@ -2,3 +2,8 @@ variable "ami_id"{
     type = string
     default = "ami-0f3c7d07486cad139"
 }
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+  }
